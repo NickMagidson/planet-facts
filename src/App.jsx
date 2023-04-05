806c8f137d78dc8a58a5e61d480d5643652c7d99
Nick Magidson <Nickmagi24@gmail.com>
2023-04-05 16:09:16 -0400

    Stars background and react-router setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-
+import React from 'react'
 
 function App() {
 
 
   return (
-    <div className="App">
-
+    <div className="App" >
+      {/* <Navbar /> */}
     </div>
   )
 }
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+// import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+
+// function Navbar() {
+//   return (
+//     <BrowserRouter>
+//       <Routes>
+//         <Route path="/"></Route>
+//         <Route>Venus</Route>
+//         <Route></Route>
+//       </Routes>
+//     </BrowserRouter>
+//   )
+// }
+
+// export default Navbar
