9a5fa1d03ff1899a0933e79233d3c7064bac184b
Nick Magidson <Nickmagi24@gmail.com>
2023-04-12 14:51:37 -0400

    Bootstrap, MDB, and sass installation. Basic mobile menu setup

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,63 @@
-import { Link } from "react-router-dom"
+// import { Link } from "react-router-dom"
 
-function Navbar() {
+// function Navbar() {
+//   return (
+//     <>
+//       <nav>
+//         <ul>
+//           <li><Link to="mercury">Mercury</Link></li>
+//           <li><Link to="venus">Venus</Link></li>
+//         </ul>
+//       </nav>
+//     </>
+//   )
+// }
+
+
+import React, { useState } from 'react';
+import {
+  MDBContainer,
+  MDBCollapse,
+  MDBNavbar,
+  MDBNavbarToggler,
+  MDBIcon,
+  MDBBtn,
+} from 'mdb-react-ui-kit';
+
+export default function Navbar() {
+  const [showNavExternal3, setShowNavExternal3] = useState(false);
+
   return (
     <>
-      <nav>
-        <ul>
-          <li><Link to="mercury">Mercury</Link></li>
-          <li><Link to="venus">Venus</Link></li>
-        </ul>
-      </nav>
-    </>
-  )
-}
+      <MDBNavbar>
+        <MDBContainer fluid>
+          <MDBNavbarToggler
+            className='ms-auto'
+            type='button'
+            data-target='#navbarToggleExternalContent'
+            aria-controls='navbarToggleExternalContent'
+            aria-expanded='false'
+            aria-label='Toggle navigation'
+            onClick={() => setShowNavExternal3(!showNavExternal3)}
+          >
+            <MDBIcon className='burger' icon='bars' fas />
+          </MDBNavbarToggler>
+        </MDBContainer>
+      </MDBNavbar>
 
-export default Navbar
+      <MDBCollapse show={showNavExternal3}>
+        <div className='bg-light shadow-3 p-4'>
+          <MDBBtn block className='border-bottom m-0' color='link'>
+            Link 1
+          </MDBBtn>
+          <MDBBtn block className='border-bottom m-0' color='link'>
+            Link 2
+          </MDBBtn>
+          <MDBBtn block className='border-bottom m-0' color='link'>
+            Link 2
+          </MDBBtn>
+        </div>
+      </MDBCollapse>
+    </>
+  );
+}
--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'mdb-react-ui-kit/dist/css/mdb.min.css';
+import "@fortawesome/fontawesome-free/css/all.min.css";
 import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
