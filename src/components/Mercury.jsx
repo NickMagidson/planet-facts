cb5960aebe7b332626028796608008858d3dda86
Nick Magidson <Nickmagi24@gmail.com>
2023-04-26 14:41:35 -0400

    Mercury component tablet responsive progress

--- src/components/Mercury.jsx
+++ src/components/Mercury.jsx
@@ -14,37 +14,35 @@ const Mercury = () => {
 
   return (
     <>
-      <div>
-        {/* Try routing!? */}
-        <MDBBtnGroup className="w-100 bg-transparent">
-          <MDBBtn className="planet-btn bg-transparent text-white-50">
-            <MDBTypography tag='p' className="planet-btn m-0 p-2">Overview</MDBTypography>
+        <MDBBtnGroup className=" btn-group w-100 bg-transparent">
+          <MDBBtn className="planet-btn w-100 bg-transparent text-white-50">
+            <MDBTypography tag='p' className="planet-btn m-0 p-2"><span className="number d-xs-none text-white-50">01</span>    Overview</MDBTypography>
           </MDBBtn>
-          <MDBBtn className="planet-btn bg-transparent text-white-50">
-            <MDBTypography tag='p' className="planet-btn m-0 p-2">Structure</MDBTypography>
+          <MDBBtn className="planet-btn w-100 bg-transparent text-white-50">
+            <MDBTypography tag='p' className="planet-btn m-0 p-2"><span className="number d-xs-none text-white-50">02</span>   Structure</MDBTypography>
           </MDBBtn>
-          <MDBBtn className="planet-btn bg-transparent text-white-50">
-            <MDBTypography tag='p' className="planet-btn m-0 p-2">Geology</MDBTypography>
+          <MDBBtn className="planet-btn w-100 bg-transparent text-white-50">
+            <MDBTypography tag='p' className="planet-btn m-0 p-2"><span className="number d-xs-none text-white-50">03</span>   Geology</MDBTypography>
           </MDBBtn>
         </MDBBtnGroup>
-      </div>
-      <hr style={{borderTop: '1px solid #bbb', margin: '0'}} ></hr>
+    
+      <hr className="d-sm-none" style={{borderTop: '1px solid #bbb', margin: '0'}} ></hr>
       <main className="d-flex flex-column align-items-center h-100">
 
         <div className="img-container m-0 w-100 d-flex justify-content-center align-items-center">
-          <img src={images.planet} className="w-25" alt="planet" />
+          <img src={images.planet} className="w-25 planet-img" alt="planet" />
         </div>
 
         <section className="text-section">
 
-          <div className="d-flex flex-column align-items-center">
+          <div className="text-blurb d-flex flex-column align-items-center">
             <h1 className="planet-name m-0">{name.toUpperCase()}</h1>
             <MDBTypography tag='p' className="planet-desc p-4 text-center">{overview.content}</MDBTypography>
             <MDBTypography tag='p' className="planet-source text-sm-start">Source: <span>Wikipedia <img src={source} /></span></MDBTypography>
           </div>
 
       
-            <MDBRow className="mb-3 m-0 p-4 h-0">
+            <MDBRow className="info-grid mb-3 m-0 p-4 h-0">
               <MDBCol size='md' className="d-flex flex-row justify-content-between align-items-center p-2 border border-dark">
                 <MDBTypography tag='p' className="planet-grid-title m-0">ROTATION TIME</MDBTypography>
                 <MDBTypography tag='h3' className="planet-grid-info m-0">{rotation}</MDBTypography>
