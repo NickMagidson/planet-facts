16c4be51a36785186513046491f514483534d65a
Nick Magidson <Nickmagi24@gmail.com>
2023-05-08 11:56:08 -0400

    Refactor: used .Map() method for navbar Links. And added icon colors to json file

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -45,14 +45,6 @@ export default function Navbar() {
           {planets.map(planetName => {
             return <Link style={linkStyles} className='nav-link text-light' to={'/' + planetName.name}>{planetName.name.toUpperCase()}</Link>
           })}
-            {/* <Link style={linkStyles} className='nav-link text-light' to='/mercury'>MERCURY</Link>           
-            <Link style={linkStyles} className='nav-link text-light' to='/venus'>VENUS</Link>    
-            <Link style={linkStyles} className='nav-link text-light' to='/earth'>EARTH</Link>           
-            <Link style={linkStyles} className='nav-link text-light' to='/mars'>MARS</Link>    
-            <Link style={linkStyles} className='nav-link text-light' to='/jupiter'>JUPITER</Link>                 
-            <Link style={linkStyles} className='nav-link text-light' to='/saturn'>SATURN</Link>    
-            <Link style={linkStyles} className='nav-link text-light' to='/uranus'>URANUS</Link>    
-            <Link style={linkStyles} className='nav-link text-light' to='/neptune'>NEPTUNE</Link>     */}
           </nav>
         </MDBContainer>
       </MDBNavbar>
@@ -61,71 +53,17 @@ export default function Navbar() {
       <MDBCollapse className='navbar-expand-md' show={showNavExternal3}>
         <div className='bg-transparent shadow-3 p-4'>
 
-          {/****** MERCURY ******/}
-          <MDBBtn block className='btn border-bottom border-dark m-0 p-2 d-flex align-items-center justify-content-between' color='link'>
-            <div className='d-inline-flex justify-content-between align-items-center'>
-              <span className='dot'></span>
-              <Link style={{ fontSize: "1.2rem" }} className='nav-link text-light' to='/mercury'>Mercury</Link>
-            </div>    
-            <img src={Arrow} />         
-          </MDBBtn>
-          {/****** VENUS ******/}
-          <MDBBtn block className='border-bottom border-dark m-0 p-2 d-flex align-items-center justify-content-between' color='link'>
-            <div className='d-inline-flex justify-content-between align-items-center'>
-              <span style={{backgroundColor: '#F7CC7F'}} className='dot'></span>
-              <Link style={{ fontSize: "1.2rem" }} className='nav-link text-light' to='/venus'>Venus</Link>
-            </div>    
-            <img src={Arrow} />         
-          </MDBBtn>
-          {/****** EARTH ******/}
-          <MDBBtn block className='border-bottom border-dark m-0 p-2 d-flex align-items-center justify-content-between' color='link'>
-            <div className='d-inline-flex justify-content-between align-items-center'>
-              <span style={{backgroundColor: '#545BFE'}} className='dot'></span>
-              <Link style={{ fontSize: "1.2rem" }} className='nav-link text-light' to='/earth'>Earth</Link>
-            </div>    
-            <img src={Arrow} />         
-          </MDBBtn>
-          {/****** MARS ******/}
-          <MDBBtn block className='border-bottom border-dark m-0 p-2 d-flex align-items-center justify-content-between' color='link'>
-            <div className='d-inline-flex justify-content-between align-items-center'>
-              <span style={{backgroundColor: '#FF6A45'}} className='dot'></span>
-              <Link style={{ fontSize: "1.2rem" }} className='nav-link text-light' to='/mars'>Mars</Link>
-            </div>    
-            <img src={Arrow} />         
-          </MDBBtn>
-          {/****** JUPITER ******/}
-          <MDBBtn block className='border-bottom border-dark m-0 p-2 d-flex align-items-center justify-content-between' color='link'>
-            <div className='d-inline-flex justify-content-between align-items-center'>
-              <span style={{backgroundColor: '#ECAD7A'}} className='dot'></span>
-              <Link style={{ fontSize: "1.2rem" }} className='nav-link text-light' to='/jupiter'>Jupiter</Link>
-            </div>    
-            <img src={Arrow} />         
-          </MDBBtn>
-          {/****** SATURN ******/}
-          <MDBBtn block className='border-bottom border-dark m-0 p-2 d-flex align-items-center justify-content-between' color='link'>
-            <div className='d-inline-flex justify-content-between align-items-center'>
-              <span style={{backgroundColor: '#FCCB6B'}} className='dot'></span>
-              <Link style={{ fontSize: "1.2rem" }} className='nav-link text-light' to='/saturn'>Saturn</Link>
-            </div>    
-            <img src={Arrow} />         
-          </MDBBtn>
-          {/****** URANUS ******/}
-          <MDBBtn block className='border-bottom border-dark m-0 p-2 d-flex align-items-center justify-content-between' color='link'>
-            <div className='d-inline-flex justify-content-between align-items-center'>
-              <span style={{backgroundColor: '#65F0D5'}} className='dot'></span>
-              <Link style={{ fontSize: "1.2rem" }} className='nav-link text-light' to='/uranus'>Uranus</Link>
-            </div>    
-            <img src={Arrow} />         
-          </MDBBtn>
-          {/****** NEPTUNE ******/}
-          <MDBBtn block className='border-bottom border-dark m-0 p-2 d-flex align-items-center justify-content-between' color='link'>
-            <div className='d-inline-flex justify-content-between align-items-center'>
-              <span style={{backgroundColor: '#497EFA'}} className='dot'></span>
-              <Link style={{ fontSize: "1.2rem" }} className='nav-link text-light' to='/neptune'>Neptune</Link>
-            </div>    
-            <img src={Arrow} />         
-          </MDBBtn>
-
+          {planets.map(planetName => {
+            return (
+              <MDBBtn block className='btn border-bottom border-dark m-0 p-2 d-flex align-items-center justify-content-between' color='link'>
+                <div className='d-inline-flex justify-content-between align-items-center'>
+                  <span style={{ backgroundColor: planetName.color}} className='dot'></span>
+                  <Link style={{ fontSize: "1.2rem" }} className='nav-link text-light' to={'/' + planetName.name}>{planetName.name.toUpperCase()}</Link>
+                </div>    
+                <img src={Arrow} />         
+              </MDBBtn>
+            )
+          })}
         </div>
       </MDBCollapse>
       <hr style={{borderTop: '1px solid #bbb', margin: '0'}} ></hr>
