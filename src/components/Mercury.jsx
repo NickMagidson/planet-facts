1628e4fa40df8c2a54b459ef205a7c68179deb65
Nick Magidson <Nickmagi24@gmail.com>
2023-05-09 13:06:34 -0400

    Bug fix: Mobile menu not collapsing on route click

--- src/components/Mercury.jsx
+++ src/components/Mercury.jsx
@@ -43,21 +43,21 @@ const Mercury = () => {
           </div>
           <div className="tablet-info">
             <div className="text-blurb d-flex flex-column align-items-center justify-content-sm-start">
-              <h1 className="planet-name m-0 ">{name.toUpperCase()}</h1>
-              <p className="planet-desc p-4 ">{summary}</p>
-              <p className="planet-source ">Source: <span>Wikipedia <img src={source} /></span></p>
+              <h1 className="planet-name m-0">{name.toUpperCase()}</h1>
+              <p className="planet-desc p-4">{summary}</p>
+              <p className="planet-source">Source: <span src><a href={overview.source}>Wikipedia <img src={source} /></a></span></p>
             </div>
 
             {/* SUMMARY BUTTONS FOR TABLET RESPONSIVENESS */}
             <MDBBtnGroup className="btn-group-2 w-100 bg-transparent d-none d-sm-block">
-              <MDBBtn className="planet-btn w-100 bg-transparent text-white-50">
-                <p tag='p' className="planet-btn m-0 p-2"><span className="number d-xs-none text-white-50">01</span>    Overview</p>
+              <MDBBtn className="planet-btn w-100 bg-transparent text-white-50" onClick={() => handleSummary(overview.content)}>
+                <p className="planet-btn m-0 p-2"><span className="number d-xs-none text-white-50">01</span>    Overview</p>
               </MDBBtn>
-              <MDBBtn className="planet-btn w-100 bg-transparent text-white-50">
-                <p tag='p' className="planet-btn m-0 p-2"><span className="number d-xs-none text-white-50">02</span>   Structure</p>
+              <MDBBtn className="planet-btn w-100 bg-transparent text-white-50" onClick={() => handleSummary(structure.content)}>
+                <p className="planet-btn m-0 p-2"><span className="number d-xs-none text-white-50">02</span>   Structure</p>
               </MDBBtn>
-              <MDBBtn className="planet-btn w-100 bg-transparent text-white-50">
-                <p tag='p' className="planet-btn m-0 p-2"><span className="number d-xs-none text-white-50">03</span>   Geology</p>
+              <MDBBtn className="planet-btn w-100 bg-transparent text-white-50" onClick={() => handleSummary(geology.content)}>
+                <p className="planet-btn m-0 p-2"><span className="number d-xs-none text-white-50">03</span>   Geology</p>
               </MDBBtn>
             </MDBBtnGroup>
             
@@ -66,23 +66,23 @@ const Mercury = () => {
 
             <MDBRow className="info-grid p-4 h-0">
               <MDBCol size='md' className="grid-block p-2 border border-dark gy-3">
-                <p tag='p' className="planet-grid-title m-0 w-100">ROTATION TIME</p>
-                <h3 tag='h3' className="planet-grid-info m-0 w-100">{rotation}</h3>
+                <p className="planet-grid-title m-0 w-100">ROTATION TIME</p>
+                <h3 className="planet-grid-info m-0 w-100">{rotation}</h3>
               </MDBCol>
 
               <MDBCol size='md' className="grid-block p-2 border border-dark gy-3">
-                <p tag='p' className="planet-grid-title m-0 w-100">REVOLUTION TIME</p>
-                <h3 tag='h3' className="planet-grid-info m-0 w-100">{revolution}</h3>
+                <p className="planet-grid-title m-0 w-100">REVOLUTION TIME</p>
+                <h3 className="planet-grid-info m-0 w-100">{revolution}</h3>
               </MDBCol>
 
               <MDBCol size='md' className="grid-block p-2 border border-dark gy-3">
-                <p tag='p' className="planet-grid-title m-0 w-100">RADIUS</p>
-                <h3 tag='h3' className="planet-grid-info m-0 w-100">{radius}</h3>
+                <p className="planet-grid-title m-0 w-100">RADIUS</p>
+                <h3 className="planet-grid-info m-0 w-100">{radius}</h3>
               </MDBCol>
 
               <MDBCol size='md' className="grid-block p-2 border border-dark gy-3">
-                <p tag='p' className="planet-grid-title m-0 w-100">AVERAGE TEMP.</p>
-                <h3 tag='h3' className="planet-grid-info m-0 w-100">{temperature}</h3>
+                <p className="planet-grid-title m-0 w-100">AVERAGE TEMP.</p>
+                <h3 className="planet-grid-info m-0 w-100">{temperature}</h3>
               </MDBCol>
             </MDBRow>
          
