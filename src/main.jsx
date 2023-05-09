ee807bc218fd37405af6ed3bbc89fd752e91fd69
Nick Magidson <Nickmagi24@gmail.com>
2023-05-09 14:51:01 -0400

    Commit recovery and uninstalled gh-pages

--- src/main.jsx
+++ src/main.jsx
@@ -9,7 +9,7 @@ import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </React.StrictMode>,
