06b572b6fb404278b1bbdf2238310e49b32fdd79
Nick Magidson <Nickmagi24@gmail.com>
2023-05-12 16:36:27 -0400

    HashRouter

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import './index.scss'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'mdb-react-ui-kit/dist/css/mdb.min.css';
 import "@fortawesome/fontawesome-free/css/all.min.css";
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
