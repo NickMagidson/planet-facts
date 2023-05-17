a3a4e4e7598c2d1b29ffa9a86a714de30628b9b1
Nick <nickmagi24@gmail.com>
2023-05-17 14:25:27 -0400

    Refactor: SolarSystem component jsx render with .map()

--- src/components/SolarSystem/SolarSystem.jsx
+++ src/components/SolarSystem/SolarSystem.jsx
@@ -5,20 +5,8 @@ const SolarSystem = () => {
   return (
     <>
       <div className='solar-syst'>
-
-        {planets.map((planetName) => {
-          return <div className={planetName.name.toLowerCase()}></div>
-        })}
-
-        {/* <div className='sun'></div>
-        <div className='mercury'></div>
-        <div className='venus'></div>
-        <div className='earth'></div>
-        <div className='mars'></div>
-        <div className='jupiter'></div>
-        <div className='saturn'></div>
-        <div className='uranus'></div>
-        <div className='neptune'></div> */}
+        <div className='sun'></div>
+        {planets.map((planetName) => {return <div className={planetName.name.toLowerCase()}></div>})}
         <div className='asteroids-belt'></div>
       </div>
     </>
