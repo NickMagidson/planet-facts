5caf26902e685e8763076c270d9e2f639a38ec00
Nick Magidson <Nickmagi24@gmail.com>
2023-05-30 12:01:34 -0400

    Refactor: Converted class component into functional component

--- src/components/AstroPhoto/AstroPhoto.jsx
+++ src/components/AstroPhoto/AstroPhoto.jsx
@@ -1,76 +1,54 @@
-import React, { Component, useState } from "react";
+import React, { useState, useEffect } from "react";
 import Photo from "./Photo";
 import DateInput from "./DateInput";
 import CircleLoader from "react-spinners/CircleLoader";
 import '../AstroPhoto/photoStyle.scss';
 
-
 const API_KEY = 'V3So8Qu3NHWIE20l3VCTlXhyZscIKTZK7W1vhJS8';
-// const API_URL = `https://api.nasa.gov/planetary/apod?api_key=${API_KEY}`;
 
-// const [loading, setLoading] = useState(true);
+const AstroPhoto = () => {
+  const [loading, setLoading] = useState(true);
+  const [photo, setPhoto] = useState('');
+  const [date, setDate] = useState(new Date());
 
-
-class AstroPhoto extends Component {
-  // const [loading, setLoading] = useState(true);
-  // const [photo, setPhoto] = useState('')
-  // const [date, setDate] = useState(new Date())
-
-  state = {
-		date: new Date(),
-		photo: ''
-	}
-
-	formatDate = (date) => {
-		// converts date to yyyy-mm-dd
-		return date.toISOString().split('T')[0]
-	  }
-
-
-	changeDate = dateFromInput => {
-		this.setState({ date: dateFromInput });
-		this.getPhotoByDate(this.formatDate(dateFromInput)); 
-	}
-
-	getPhotoByDate = date => {
-		fetch(`https://api.nasa.gov/planetary/apod?date=${date}&api_key=${API_KEY}`)
-		.then((response) => {
-		  return response.json()
-		})
-		.then((photoData) => {
-		  this.setState({ photo: photoData })
-		})
-	};
-
-  componentDidMount() {
-    fetch(`https://api.nasa.gov/planetary/apod?api_key=${API_KEY}`)
-      .then((response) => {
-        return response.json()
-      })
-      .then((json) => {
-        this.setState({ photo: json })
-      })
+  const formatDate = (date) => {
+    return date.toISOString().split('T')[0];
   };
 
-	render() {
-    return (
-      <>
-        <main id="main" style={{ overflow: 'scroll', height: '100vh' }}>
+  const changeDate = (dateFromInput) => {
+    setDate(dateFromInput);
+    getPhotoByDate(formatDate(dateFromInput));
+  };
 
-          <DateInput 
-            date={this.state.date}
-            changeDate={this.changeDate}
-            handleClick={this.handleClick} 
-          />
-          <hr className="d-sm-none" style={{borderTop: '1px solid #bbb', margin: '0'}} ></hr>
-    
-          <Photo photo={this.state.photo} />
+  const getPhotoByDate = (date) => {
+    fetch(`https://api.nasa.gov/planetary/apod?date=${date}&api_key=${API_KEY}`)
+    .then((response) => response.json())
+    .then((photoData) => {
+    setPhoto(photoData);
+    });
+  };
 
-          {/* <CircleLoader color="#36d7b7" />  */}
+  useEffect(() => {
+    fetch(`https://api.nasa.gov/planetary/apod?api_key=${API_KEY}`)
+    .then((response) => response.json())
+    .then((json) => {
+    setPhoto(json);
+    });
+  }, []);
 
-        </main>
-      </>
-    )
-	};
-};
+
+  return (
+    <>
+      <main id="main" style={{ overflow: 'scroll', height: '100vh' }}>
+        <DateInput
+          date={date}
+          changeDate={changeDate}
+        />
+        <hr className="d-sm-none" style={{ borderTop: '1px solid #bbb', margin: '0' }}></hr>
+        <Photo photo={photo} />
+      {/* <CircleLoader color="#36d7b7" /> */}
+      </main>
+    </>
+  );
+}
 export default AstroPhoto;
