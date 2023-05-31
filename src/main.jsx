db17b0fdd9f5f526c59da4d6412e12980ef4bb19
Nick Magidson <Nickmagi24@gmail.com>
2023-05-31 11:14:04 -0400

    Updated main.jsx for new sass directory

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import './index.scss'
+import './sass/index.scss'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'mdb-react-ui-kit/dist/css/mdb.min.css';
 import "@fortawesome/fontawesome-free/css/all.min.css";
