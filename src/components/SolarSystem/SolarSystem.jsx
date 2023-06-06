6b981cb3138b14b0b9cc97b74fedee23b96a70a4
Nick Magidson <Nickmagi24@gmail.com>
2023-06-06 12:18:30 -0400

    Code cleanup

--- src/components/SolarSystem/SolarSystem.jsx
+++ src/components/SolarSystem/SolarSystem.jsx
@@ -7,7 +7,18 @@ const SolarSystem = () => {
     <>
       <div className='solar-syst'>
         <div className='sun'></div>
-        {planets.map((planetName, index) => {return <Link to={'/' + planetName.name} className={planetName.name + '-link'}><div key={index} className={!planetName.photo && planetName.name.toLowerCase()}></div></Link>})}
+        {planets.map((planetName, index) => { 
+          return ( 
+            <Link 
+              to={'/' + planetName.name} 
+              className={planetName.name + '-link'}>
+                <div 
+                  key={index} 
+                  className={!planetName.photo && planetName.name.toLowerCase()}>
+                </div>
+            </Link>
+          )})
+        }
         <div className='asteroids-belt'></div>
       </div>
     </>
