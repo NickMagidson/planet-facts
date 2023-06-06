07eaaee97d426d443e95755d52c6552ac3cecfc5
Nick Magidson <Nickmagi24@gmail.com>
2023-06-06 11:49:02 -0400

    Implemented solar system component page routing and basic hover effects

--- src/components/SolarSystem/SolarSystem.jsx
+++ src/components/SolarSystem/SolarSystem.jsx
@@ -1,12 +1,13 @@
 import './solarSystem.scss'
 import planets from '../../data.json'
+import { Link } from 'react-router-dom'
 
 const SolarSystem = () => {
   return (
     <>
       <div className='solar-syst'>
         <div className='sun'></div>
-        {planets.map((planetName, index) => {return <div key={index} className={!planetName.photo && planetName.name.toLowerCase()}></div>})}
+        {planets.map((planetName, index) => {return <Link to={'/' + planetName.name} className={planetName.name + '-link'}><div key={index} className={!planetName.photo && planetName.name.toLowerCase()}></div></Link>})}
         <div className='asteroids-belt'></div>
       </div>
     </>
