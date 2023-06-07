c6e964dcaac5e34d862cd5d3977e16f964818c41
Nick Magidson <Nickmagi24@gmail.com>
2023-06-07 11:16:15 -0400

    Solar system div container and star absolute positoning edits (still in progress)

--- src/components/SolarSystem/SolarSystem.jsx
+++ src/components/SolarSystem/SolarSystem.jsx
@@ -5,22 +5,24 @@ import { Link } from 'react-router-dom'
 const SolarSystem = () => {
   return (
     <>
-      <div className='solar-syst'>
-        <div className='sun'></div>
-        {planets.map((planetName, index) => { 
-          return ( 
-            <Link 
-              to={'/' + planetName.name} 
-              className={planetName.name + '-link'}>
-                <div 
-                  key={index} 
-                  className={!planetName.photo && planetName.name.toLowerCase()}>
-                </div>
-            </Link>
-          )})
-        }
-        <div className='asteroids-belt'></div>
-      </div>
+      <main id='space-container'>
+        <div className='solar-syst'>
+          <div className='sun'></div>
+          {planets.map((planetName, index) => { 
+            return ( 
+              <Link 
+                to={'/' + planetName.name} 
+                className={planetName.name + '-link'}>
+                  <div 
+                    key={index} 
+                    className={!planetName.photo && planetName.name.toLowerCase()}>
+                  </div>
+              </Link>
+            )})
+          }
+          <div className='asteroids-belt'></div>
+        </div>
+      </main>
     </>
   )
 }
